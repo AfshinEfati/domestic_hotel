8b1c791ec26f90f9d6871e3b3b0496e46b60cc23
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 09:38:09 +0330

    feat: register provider pricing rule service

--- app/Providers/HotelPricingServiceProvider.php
+++ app/Providers/HotelPricingServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Repositories\Contracts\ProviderPricingRuleRepositoryInterface;
 use App\Repositories\Eloquent\ProviderPricingRuleRepository;
 use App\Services\Contracts\HotelRatePricingServiceInterface;
+use App\Services\Contracts\ProviderPricingRuleServiceInterface;
 use App\Services\HotelRatePricingService;
+use App\Services\ProviderPricingRuleService;
 use Illuminate\Support\ServiceProvider;
 
 class HotelPricingServiceProvider extends ServiceProvider
@@ -13,6 +15,7 @@ class HotelPricingServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ProviderPricingRuleRepositoryInterface::class, ProviderPricingRuleRepository::class);
+        $this->app->bind(ProviderPricingRuleServiceInterface::class, ProviderPricingRuleService::class);
         $this->app->bind(HotelRatePricingServiceInterface::class, HotelRatePricingService::class);
     }
 }
