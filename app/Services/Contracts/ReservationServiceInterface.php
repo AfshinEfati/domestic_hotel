7c2085643e4f3d31f45d1ed110ae3357c96a0f28
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 09:34:34 +0330

    feat: add reservation service contract

---
+++ app/Services/Contracts/ReservationServiceInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services\Contracts;
+
+use App\Models\Reservation;
+use App\Models\ReservationHotel;
+use App\Models\ReservationPurchaseSegment;
+use App\Models\ReservationRoom;
+
+interface ReservationServiceInterface extends BaseServiceInterface
+{
+    public function store(mixed $payload): Reservation;
+
+    public function findByReservationNumber(string $reservationNumber): ?Reservation;
+
+    public function changeStatus(int $reservationId, int $status): bool;
+
+    public function addHotel(int $reservationId, int $accommodationId, int $type): ReservationHotel;
+
+    public function setFinalHotel(int $reservationId, int $reservationHotelId): ReservationHotel;
+
+    public function addRoom(int $reservationHotelId, mixed $payload): ReservationRoom;
+
+    public function addPurchaseSegment(int $reservationRoomId, mixed $payload): ReservationPurchaseSegment;
+}
