e11fb366a3428c10d327f8d26bd8fb118f80433e
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 10:13:44 +0330

    feat: register hotel settings seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             ProviderSeeder::class,
+            HotelSettingSeeder::class,
+            ProviderPricingRuleSeeder::class,
         ]);
     }
 }
