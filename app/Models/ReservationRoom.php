fdc03713b0849285f1143ae2bebeb3ae7da6cea8
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 09:29:41 +0330

    feat: add reservation room model

---
+++ app/Models/ReservationRoom.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ReservationRoom extends Model
+{
+    protected $fillable = [
+        'reservation_hotel_id',
+        'room_type_id',
+        'rate_plan_id',
+        'room_name',
+        'quantity',
+        'check_in',
+        'check_out',
+    ];
+
+    protected $casts = [
+        'reservation_hotel_id' => 'integer',
+        'room_type_id' => 'integer',
+        'rate_plan_id' => 'integer',
+        'quantity' => 'integer',
+        'check_in' => 'date',
+        'check_out' => 'date',
+    ];
+
+    public function reservationHotel(): BelongsTo
+    {
+        return $this->belongsTo(ReservationHotel::class);
+    }
+
+    public function roomType(): BelongsTo
+    {
+        return $this->belongsTo(RoomType::class);
+    }
+
+    public function ratePlan(): BelongsTo
+    {
+        return $this->belongsTo(RatePlan::class);
+    }
+
+    public function purchaseSegments(): HasMany
+    {
+        return $this->hasMany(ReservationPurchaseSegment::class);
+    }
+}
