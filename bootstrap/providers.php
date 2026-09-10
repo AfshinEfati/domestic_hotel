60bb8adaffa50f3e5001bc3d074b22253aeabda7
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 09:36:18 +0330

    feat: register reservation and pricing providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,7 +5,9 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\HotelChildPolicyServiceProvider::class,
+    App\Providers\HotelPricingServiceProvider::class,
     App\Providers\HotelServiceProvider::class,
+    App\Providers\ReservationServiceProvider::class,
     App\Providers\RoomTypeNameServiceProvider::class,
     App\Providers\RuleServiceProvider::class,
 ];
