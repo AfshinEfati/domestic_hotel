d22505341de08df80f40affcd5c09e5cbca6ce56
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-10 09:42:19 +0330

    feat: add configurable reservation reference format

--- config/hotel.php
+++ config/hotel.php
@@ -6,6 +6,11 @@ return [
         'default_fixed_amount' => (int) env('HOTEL_DEFAULT_MARKUP_FIXED_AMOUNT', 0),
     ],
 
+    'reservation' => [
+        'reference_prefix' => env('HOTEL_RESERVATION_REFERENCE_PREFIX', 'DH'),
+        'reference_random_length' => (int) env('HOTEL_RESERVATION_REFERENCE_RANDOM_LENGTH', 10),
+    ],
+
     'providers' => [
         'grs' => [
             'availability' => [
