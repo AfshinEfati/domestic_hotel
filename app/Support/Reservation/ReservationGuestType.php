a2b0af19a922f26c264f1663a6780b7d1341e662
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-12 15:11:36 +0330

    feat: add reservation guest type support

---
+++ app/Support/Reservation/ReservationGuestType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Support\Reservation;
+
+final class ReservationGuestType
+{
+    public const ADULT = 1;
+    public const CHILD = 2;
+    public const INFANT = 3;
+
+    public static function all(): array
+    {
+        return [
+            self::ADULT,
+            self::CHILD,
+            self::INFANT,
+        ];
+    }
+
+    public static function isValid(int $type): bool
+    {
+        return in_array($type, self::all(), true);
+    }
+
+    public static function options(): array
+    {
+        return [
+            self::ADULT => ['name' => 'adult', 'code' => self::ADULT],
+            self::CHILD => ['name' => 'child', 'code' => self::CHILD],
+            self::INFANT => ['name' => 'infant', 'code' => self::INFANT],
+        ];
+    }
+
+    public static function get(int $type): ?array
+    {
+        return self::options()[$type] ?? null;
+    }
+}
