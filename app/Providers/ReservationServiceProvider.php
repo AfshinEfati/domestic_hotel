87723401faeefd1ea8f5515a5d3fa1b3192756ea
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-16 09:23:06 +0330

    feat: bind reservation guest repository

--- app/Providers/ReservationServiceProvider.php
+++ app/Providers/ReservationServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\ReservationGuestRepositoryInterface;
 use App\Repositories\Contracts\ReservationHotelRepositoryInterface;
 use App\Repositories\Contracts\ReservationPurchaseSegmentRepositoryInterface;
 use App\Repositories\Contracts\ReservationRepositoryInterface;
 use App\Repositories\Contracts\ReservationRoomRepositoryInterface;
+use App\Repositories\Eloquent\ReservationGuestRepository;
 use App\Repositories\Eloquent\ReservationHotelRepository;
 use App\Repositories\Eloquent\ReservationPurchaseSegmentRepository;
 use App\Repositories\Eloquent\ReservationRepository;
@@ -23,6 +25,7 @@ class ReservationServiceProvider extends ServiceProvider
         $this->app->bind(ReservationRepositoryInterface::class, ReservationRepository::class);
         $this->app->bind(ReservationHotelRepositoryInterface::class, ReservationHotelRepository::class);
         $this->app->bind(ReservationRoomRepositoryInterface::class, ReservationRoomRepository::class);
+        $this->app->bind(ReservationGuestRepositoryInterface::class, ReservationGuestRepository::class);
         $this->app->bind(ReservationPurchaseSegmentRepositoryInterface::class, ReservationPurchaseSegmentRepository::class);
         $this->app->bind(ReservationReferenceGeneratorInterface::class, ReservationReferenceGenerator::class);
         $this->app->bind(ReservationServiceInterface::class, ReservationService::class);
