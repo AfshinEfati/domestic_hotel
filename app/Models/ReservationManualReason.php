ad1ad02551abc8c436cd33fec54b9894328b753b
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-16 10:50:12 +0330

    fix: use reservation id in manual reason model relation

--- app/Models/ReservationManualReason.php
+++ app/Models/ReservationManualReason.php
@@ -8,22 +8,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ReservationManualReason extends Model
 {
     protected $fillable = [
-        'reservation_number',
-        'provider_id',
+        'reservation_id',
         'reason',
     ];
 
     protected $casts = [
-        'provider_id' => 'integer',
+        'reservation_id' => 'integer',
     ];
 
     public function reservation(): BelongsTo
     {
-        return $this->belongsTo(Reservation::class, 'reservation_number', 'reservation_number');
-    }
-
-    public function provider(): BelongsTo
-    {
-        return $this->belongsTo(Provider::class);
+        return $this->belongsTo(Reservation::class);
     }
 }
