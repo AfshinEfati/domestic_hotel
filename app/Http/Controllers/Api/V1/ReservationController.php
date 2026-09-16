1b3d0d5cb9ce7ef6813159cc5be88556b52095ec
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-16 09:24:33 +0330

    feat: add reservation create controller

---
+++ app/Http/Controllers/Api/V1/ReservationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Helpers\StatusHelper;
+use App\Http\Requests\StoreReservationRequest;
+use App\Http\Resources\ReservationResource;
+use App\Services\Contracts\ReservationServiceInterface;
+use Illuminate\Http\JsonResponse;
+
+class ReservationController
+{
+    public function __construct(
+        public ReservationServiceInterface $service
+    ) {}
+
+    public function store(StoreReservationRequest $request): JsonResponse
+    {
+        $reservation = $this->service->createRequest($request->validated());
+
+        return StatusHelper::successResponse(
+            new ReservationResource($reservation),
+            'reservation created',
+            201
+        );
+    }
+}
