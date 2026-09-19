d0d87b58b093ee9f4c090f330178b992ee7ae157
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-19 13:32:05 +0330

    refactor(grs): expose accommodation-to-provider mapping lookup

--- app/Repositories/Contracts/AccommodationProviderMapRepositoryInterface.php
+++ app/Repositories/Contracts/AccommodationProviderMapRepositoryInterface.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Collection;
 
 interface AccommodationProviderMapRepositoryInterface extends BaseRepositoryInterface
 {
-    /**
-     * @return iterable<AccommodationProviderMap>
-     */
+    /** @return iterable<AccommodationProviderMap> */
     public function getAll(): iterable;
 
     public function find(int|string $id): ?AccommodationProviderMap;
+
+    public function findForAccommodationAndProvider(int $accommodationId, int $providerId): ?AccommodationProviderMap;
 
     public function store(array $data): AccommodationProviderMap;
 
@@ -24,9 +24,7 @@ interface AccommodationProviderMapRepositoryInterface extends BaseRepositoryInte
 
     public function existsForAccommodationAndProvider(int $accommodationId, int $providerId): bool;
 
-    /**
-     * @param callable(Collection<int, AccommodationProviderMap>): void $callback
-     */
+    /** @param callable(Collection<int, AccommodationProviderMap>): void $callback */
     public function chunkMappedPropertiesByProvider(int $providerId, int $chunkSize, callable $callback): void;
 
     public function chunkByProvider(int $providerId, callable $callback): void;
