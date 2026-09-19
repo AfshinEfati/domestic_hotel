3dafe4261a66a2fd438f10f89af779a0b38b492c
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-19 13:39:03 +0330

    refactor(hotel): move room mapping lookup from service to repository

--- app/Repositories/Eloquent/RoomTypeProviderMapRepository.php
+++ app/Repositories/Eloquent/RoomTypeProviderMapRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquent;
 
 use App\Models\RoomTypeProviderMap;
 use App\Repositories\Contracts\RoomTypeProviderMapRepositoryInterface;
+use Illuminate\Support\Collection;
 
 class RoomTypeProviderMapRepository extends BaseRepository implements RoomTypeProviderMapRepositoryInterface
 {
@@ -12,9 +13,7 @@ class RoomTypeProviderMapRepository extends BaseRepository implements RoomTypePr
         parent::__construct($model);
     }
 
-    /**
-     * @return iterable<RoomTypeProviderMap>
-     */
+    /** @return iterable<RoomTypeProviderMap> */
     public function getAll(): iterable
     {
         /** @var iterable<RoomTypeProviderMap> */
@@ -25,6 +24,19 @@ class RoomTypeProviderMapRepository extends BaseRepository implements RoomTypePr
     {
         /** @var RoomTypeProviderMap|null */
         return parent::find($id);
+    }
+
+    public function mappedForProviderIds(int $providerId, array $providerRoomIds): Collection
+    {
+        if ($providerRoomIds === []) {
+            return collect();
+        }
+
+        return $this->model->newQuery()
+            ->where('provider_id', $providerId)
+            ->whereIn('provider_room_type_id', $providerRoomIds)
+            ->get()
+            ->keyBy('provider_room_type_id');
     }
 
     public function store(array $data): RoomTypeProviderMap
