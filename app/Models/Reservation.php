9dec7503451a3b738c44a345742d014246aaee5f
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-26 10:49:59 +0330

    feat(reservation): add provider request relation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -60,4 +60,9 @@ class Reservation extends Model
     {
         return $this->hasMany(ReservationManualReason::class);
     }
+
+    public function providerRequests(): HasMany
+    {
+        return $this->hasMany(ProviderRequest::class);
+    }
 }
