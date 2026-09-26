84b6448285b55d460808e57c44c250f167d33a0e
afshin <afshin.efati@gmail.com>
2026-09-26 10:35:13 +0330

    chore: update project - 2026-09-26 10:35:13 +0330
    Auto commit created by git gup.
    
    Branch: main
    Date: 2026-09-26 10:35:13 +0330
    
    Changed files:
    
    Modified files:
    - app/Http/Resources/RatePlanResource.php

--- app/Http/Resources/RatePlanResource.php
+++ app/Http/Resources/RatePlanResource.php
@@ -3,8 +3,10 @@
 namespace App\Http\Resources;
 
 use App\Helpers\StatusHelper;
+use App\Models\RatePlan;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @mixin RatePlan */
 class RatePlanResource extends JsonResource
 {
     public function toArray($request): array
@@ -14,6 +16,7 @@ class RatePlanResource extends JsonResource
             'accommodation_id' => $this->accommodation_id,
             'fa_name' => $this->fa_name,
             'en_name' => $this->en_name,
+            'is_foreign_guest'=>$this->is_foreign_guest,
             'created_at' => StatusHelper::formatDates($this->created_at),
             'updated_at' => StatusHelper::formatDates($this->updated_at),
             'accommodation' => AccommodationResource::make($this->whenLoaded('accommodation')),
