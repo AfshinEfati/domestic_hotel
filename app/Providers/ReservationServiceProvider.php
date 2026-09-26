8e0fc579ec02f451ab35c836e22604153d54896a
Afshin <39827008+AfshinEfati@users.noreply.github.com>
2026-09-26 13:50:05 +0330

    feat(purchase): bind purchase request services

--- app/Providers/ReservationServiceProvider.php
+++ app/Providers/ReservationServiceProvider.php
@@ -6,7 +6,9 @@ use App\Repositories\Contracts\ProviderCreditBalanceRepositoryInterface;
 use App\Repositories\Contracts\PurchaseManualRuleRepositoryInterface;
 use App\Repositories\Contracts\ReservationGuestRepositoryInterface;
 use App\Repositories\Contracts\ReservationHotelRepositoryInterface;
+use App\Repositories\Contracts\ReservationManualPurchaseRepositoryInterface;
 use App\Repositories\Contracts\ReservationManualReasonRepositoryInterface;
+use App\Repositories\Contracts\ReservationPurchaseRepositoryInterface;
 use App\Repositories\Contracts\ReservationPurchaseSegmentRepositoryInterface;
 use App\Repositories\Contracts\ReservationRepositoryInterface;
 use App\Repositories\Contracts\ReservationRoomNightRepositoryInterface;
@@ -15,15 +17,19 @@ use App\Repositories\Eloquent\ProviderCreditBalanceRepository;
 use App\Repositories\Eloquent\PurchaseManualRuleRepository;
 use App\Repositories\Eloquent\ReservationGuestRepository;
 use App\Repositories\Eloquent\ReservationHotelRepository;
+use App\Repositories\Eloquent\ReservationManualPurchaseRepository;
 use App\Repositories\Eloquent\ReservationManualReasonRepository;
+use App\Repositories\Eloquent\ReservationPurchaseRepository;
 use App\Repositories\Eloquent\ReservationPurchaseSegmentRepository;
 use App\Repositories\Eloquent\ReservationRepository;
 use App\Repositories\Eloquent\ReservationRoomNightRepository;
 use App\Repositories\Eloquent\ReservationRoomRepository;
 use App\Services\Contracts\PurchaseResolverInterface;
+use App\Services\Contracts\ReservationPurchaseRequestServiceInterface;
 use App\Services\Contracts\ReservationReferenceGeneratorInterface;
 use App\Services\Contracts\ReservationServiceInterface;
 use App\Services\PurchaseResolver;
+use App\Services\ReservationPurchaseRequestService;
 use App\Services\ReservationReferenceGenerator;
 use App\Services\ReservationService;
 use Illuminate\Support\ServiceProvider;
@@ -37,6 +43,8 @@ class ReservationServiceProvider extends ServiceProvider
         $this->app->bind(ReservationRoomRepositoryInterface::class, ReservationRoomRepository::class);
         $this->app->bind(ReservationRoomNightRepositoryInterface::class, ReservationRoomNightRepository::class);
         $this->app->bind(ReservationGuestRepositoryInterface::class, ReservationGuestRepository::class);
+        $this->app->bind(ReservationPurchaseRepositoryInterface::class, ReservationPurchaseRepository::class);
+        $this->app->bind(ReservationManualPurchaseRepositoryInterface::class, ReservationManualPurchaseRepository::class);
         $this->app->bind(ReservationPurchaseSegmentRepositoryInterface::class, ReservationPurchaseSegmentRepository::class);
         $this->app->bind(PurchaseManualRuleRepositoryInterface::class, PurchaseManualRuleRepository::class);
         $this->app->bind(ProviderCreditBalanceRepositoryInterface::class, ProviderCreditBalanceRepository::class);
@@ -44,5 +52,6 @@ class ReservationServiceProvider extends ServiceProvider
         $this->app->bind(ReservationReferenceGeneratorInterface::class, ReservationReferenceGenerator::class);
         $this->app->bind(ReservationServiceInterface::class, ReservationService::class);
         $this->app->bind(PurchaseResolverInterface::class, PurchaseResolver::class);
+        $this->app->bind(ReservationPurchaseRequestServiceInterface::class, ReservationPurchaseRequestService::class);
     }
 }
